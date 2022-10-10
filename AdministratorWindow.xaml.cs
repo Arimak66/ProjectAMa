7b5d1888268ae8f5c1ba8f4b0b5c42359aa0bded
Ari Makkonen <ari.makkonen@pp3.inet.fi>
2022-10-10 16:47:45 +0300

    korjailua

--- AdministratorWindow.xaml.cs
+++ AdministratorWindow.xaml.cs
@@ -35,11 +35,11 @@ namespace ProjectAMa
         public class Userdata
         {
             public string Username { get; set; }
-            public int Identity { get; set; }    
+            public int Identity { get; set; }
             public string Firstname { get; set; }
             public string Lastname { get; set; }
 
-            public Userdata(string username, int identity ,string firstname, string lastname)
+            public Userdata(string username, int identity, string firstname, string lastname)
             { this.Username = username; this.Identity = identity; this.Firstname = firstname; this.Lastname = lastname; }
 
             public string UserInfo
@@ -50,84 +50,92 @@ namespace ProjectAMa
                 }
             }
         }
-            private void AddNewUser_Click(object sender, RoutedEventArgs e)
-            {
-                UserDataInsert userDataInsert = new UserDataInsert();
-                userDataInsert.Show();
-            }
-            private void CloseThisWindow_Click(object sender, RoutedEventArgs e)
-            {
-                this.Close();
-            }
-            private void ChangePassword_Click(object sender, RoutedEventArgs e)
-            {
-                ChangePassword changePassword = new ChangePassword();
-                changePassword.Show();
-            }
-            private void CreateNewCourse_Click(object sender, RoutedEventArgs e)
-            {
-                CreateNewCourse createNewCourse = new CreateNewCourse();
-                createNewCourse.Show();
-            }
-            private void ListAllUsers_Click(object sender, RoutedEventArgs e)
-            {
-                using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
-                {
-                    var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user").ToList();
-                    UserList.ItemsSource = Users;
-                }
-            }
-            private void ListAllStudents_Click(object sender, RoutedEventArgs e)
-            {
-            using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
-                {
-                    var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user where identity=1").ToList();
-                    UserList.ItemsSource = Users;
-                }
-            }
-            private void ListAllTeachers_Click(object sender, RoutedEventArgs e)
-            {
-                using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
-                {
-                    var Users = connection.Query<Userdata>($"select username,identity, firstname,lastname from user where identity=3").ToList();
-                    UserList.ItemsSource = Users;
-                }
-            }
-            private void ListAllAdministrators_Click(object sender, RoutedEventArgs e)
-            {
-                using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
-                {
-                    var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user where identity=2").ToList();
-                    UserList.ItemsSource = Users;
-                }
-            }
+        //public void InitializeUser()
+        //{
+        //    List<Userdata> Users = new List <Userdata>;
+        //    Users.Username = "";
+        //    Users.Identity = "";
+        //    Users.Firstname = "";
+        //    Users.Lastname = "";
+        //}
 
+        private void AddNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            UserDataInsert userDataInsert = new UserDataInsert();
+            userDataInsert.Show();
+        }
+        private void CloseThisWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ChangePassword_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePassword changePassword = new ChangePassword();
+            changePassword.Show();
+        }
+        private void CreateNewCourse_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewCourse createNewCourse = new CreateNewCourse();
+            createNewCourse.Show();
+        }
+        private void ListAllUsers_Click(object sender, RoutedEventArgs e)
+        {
+            //InitializeUser();
+            using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
+            {
+                var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user").ToList();
+                UserList.ItemsSource = Users;
+            }
+        }
+        private void ListAllStudents_Click(object sender, RoutedEventArgs e)
+        {
+            using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
+            {
+                var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user where identity=1").ToList();
+                UserList.ItemsSource = Users;
+            }
+        }
+        private void ListAllTeachers_Click(object sender, RoutedEventArgs e)
+        {
+            using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
+            {
+                var Users = connection.Query<Userdata>($"select username,identity, firstname,lastname from user where identity=3").ToList();
+                UserList.ItemsSource = Users;
+            }
+        }
+        private void ListAllAdministrators_Click(object sender, RoutedEventArgs e)
+        {
+            using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
+            {
+                var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user where identity=2").ToList();
+                UserList.ItemsSource = Users;
+            }
+        }
         private void SelectUser_Click(object sender, RoutedEventArgs e)
         {
-            string status;
-            Userdata userinfo= (Userdata)UserList.SelectedItems[0];
-            if(userinfo.Identity == 1) {status = "Student"; }
-            else if (userinfo.Identity == 2) { status = "Administrator";}
-            else {status = "Teacher"; }
+            try 
+            { 
+            Userdata userinfo = (Userdata)UserList.SelectedItems[0];
+                string status;
+                //if (userinfo == null) { MessageBox.Show("Select a user first"); }
+                if (userinfo.Identity == 1) { status = "Student"; }
+                else if (userinfo.Identity == 2) { status = "Administrator"; }
+                else { status = "Teacher"; }
 
-            MessageBox.Show("Valitsit käyttäjän: "+userinfo.Firstname +" "+userinfo.Lastname+" identity= "+status);
-            //using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
-            //{
-
-            //}
-
-
-
-        }
-
-        private void SC_Click(object sender, SelectionChangedEventArgs e)
-        {
+                MessageBox.Show("You selected user: " + userinfo.Firstname + " " + userinfo.Lastname + " whose identity is " + status);
+            }
+            catch { MessageBox.Show("Select first user or list of users"); }
 
         }
+            public void SC_Click(object sender, SelectionChangedEventArgs e)
+            {
+            //    //string status;
+            //    //Userdata userinfo = (Userdata)UserList.SelectedItems[0];
+            //    //if (userinfo.Identity == 1) { status = "Student"; }
+            //    //else if (userinfo.Identity == 2) { status = "Administrator"; }
+            //    //else { status = "Teacher"; }
 
-
-      
-    }
+            //    //MessageBox.Show("Valitsit käyttäjän: " + userinfo.Firstname + " " + userinfo.Lastname + " identity= " + status);
+            }
+    } 
 }
-
-
