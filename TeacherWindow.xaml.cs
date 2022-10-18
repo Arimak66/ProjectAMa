d5f291587d253518cd902b769521beca26df5a51
Ari Makkonen <ari.makkonen@pp3.inet.fi>
2022-10-18 16:32:51 +0300

    Uusittu käyttöliittymä toimii

--- TeacherWindow.xaml.cs
+++ TeacherWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectAMa.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace ProjectAMa
 {
@@ -23,7 +26,14 @@ namespace ProjectAMa
         {
             InitializeComponent();
         }
-
+        private void ListAllStudents_Click(object sender, RoutedEventArgs e)
+        {
+            using (var connection = new MySqlConnection(Helper.CnnVal("OmaDB")))
+            {
+                var Users = connection.Query<Userdata>($"select username,identity,firstname,lastname from user where identity=1").ToList();
+                UserList.ItemsSource = Users;
+            }
+        }
         private void CloseWindow_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
@@ -34,11 +44,32 @@ namespace ProjectAMa
             ChangePassword changePassword = new ChangePassword();
             changePassword.Show();
         }
+        private void SelectUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Userdata userinfo = (Userdata)UserList.SelectedItems[0];
+                string status;
+                //if (userinfo == null) { MessageBox.Show("Select a user first"); }
+                if (userinfo.Identity == 1) { status = "Student"; }
+                else if (userinfo.Identity == 2) { status = "Administrator"; }
+                else { status = "Teacher"; }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+                MessageBox.Show("You selected user: " + userinfo.Firstname + " " + userinfo.Lastname + " whose identity is " + status);
+            }
+            catch { MessageBox.Show("Select first user or list of users"); }
+        }
+
+
+        private void CreateNewCourse_Click(object sender, RoutedEventArgs e)
         {
             CreateNewCourse createNewCourse = new CreateNewCourse();
             createNewCourse.Show();
+        }
+
+        private void GradeStudent_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Under construction");
         }
     }
 }
